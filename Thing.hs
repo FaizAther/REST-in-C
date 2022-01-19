710c020383db41852cee04e1767c05e099c8a3b1
FaizAther <faizather@protonmail.com>
2022-01-20 05:38:59 +1000

    Thu 20 Jan 2022 05:38:59 AEST

--- Thing.hs
+++ Thing.hs
@@ -1,17 +1,65 @@
-{-# LANGUAGE ForeignFunctionInterface #-}
+-- {-# LANGUAGE ForeignFunctionInterface #-}
 
 module Thing where
 
-import Foreign.C ( CString, newCString )
+-- import Foreign.C ( CString, newCString )
+
+class Jonify a where
+    jonify :: a -> String
+
+preJName :: String
+preJName = "{\""
+
+midJName :: String
+midJName = "\":"
+
+postJName :: String
+postJName = "}"
+
+preJItem :: String
+preJItem = "\""
+
+postJItem :: String
+postJItem = "\""
+
+preJList :: String
+preJList = "["
+
+postJList :: String
+postJList = "]"
 
 newtype Url = Url String
             deriving (Show, Eq)
 
-data Content    = Nothing
-                | Words String
+jonifyIStr :: String -> String
+jonifyIStr xs = preJItem ++ xs ++ postJItem
+
+jonifyIsStr :: [String] -> String
+jonifyIsStr xs = preJList ++ jonifyIsStrH xs ++ postJList
+    where
+        jonifyIsStrH [] = ""
+        jonifyIsStrH [x] = jonifyIStr x
+        jonifyIsStrH (x:xs) = jonifyIStr x ++ "," ++ jonifyIsStrH xs
+
+jonifyNpStr :: String -> String
+jonifyNpStr name = preJName ++ name ++ midJName
+
+instance Jonify Url where
+    jonify (Url link) = jonifyNpStr "Url" ++ jonifyIStr link ++ postJName
+
+data Content    = Nothingg
+                | Words [String]
                 | Picture Url
                 | Video Url
+                | Audio Url
                 deriving (Show, Eq)
+
+instance Jonify Content where
+    jonify Nothingg = jonifyNpStr "Nothing" ++ postJName
+    jonify (Words ws) = jonifyNpStr "Words" ++ jonifyIsStr ws ++ postJName
+    jonify (Picture url) = jonifyNpStr "Picture" ++ jonify url ++ postJName
+    jonify (Video url) = jonifyNpStr "Video" ++ jonify url ++ postJName
+    jonify (Audio url) = jonifyNpStr "Audio" ++ jonify url ++ postJName
 
 data Duration   = Done
                 | Seconds Int Duration
@@ -36,16 +84,17 @@ newtype Element = Element (Position, Duration, Content)
 
 
 data Canvas     = One [Element]
-                | Many [Canvas] 
+                | Many [Canvas]
                 deriving (Show, Eq)
 
-canvasStr :: IO CString
-canvasStr = (newCString . show) things
 
-foreign export ccall canvasStr :: IO CString
+-- canvasStr :: IO CString
+-- canvasStr = (newCString . show) things
+
+-- foreign export ccall canvasStr :: IO CString
 
 toCanvas :: [String] -> Canvas
-toCanvas xs = One (map (\s -> Element (Position (Height 0, Width 0), Done, Words s)) xs)
+toCanvas xs = One (map (\s -> Element (Position (Height 0, Width 0), Done, Words s)) [xs])
 
 things :: Canvas
 things = toCanvas ["Hello", "This", "Is", "A", "Test"]
